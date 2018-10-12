ae8320560f8e29e7e10c50704327050dbd303bd8
Aashiq Durga <aashiq.durga@myob.com>
2018-10-12 15:32:54 +1300

    renamed tests

--- Tests/fizzbuzz.test.js
+++ Tests/fizzbuzz.test.js
@@ -1,11 +1,11 @@
 const fizzBuzz = require('../FizzBuzz');
 
-test("Given a number When the number is a multiple of 3 Then retutn Fizz ", () => {
+test("Given a number, When the number is a multiple of 3, Then return Fizz ", () => {
   var result = fizzBuzz.checkIfMultipleOfThree(3);
   expect(result).toBe('Fizz');
 });
 
-test("Given a number When the number is a multiple of 5 Then retutn Buzz ", () => {
+test("Given a number, When the number is a multiple of 5, Then return Buzz ", () => {
   var result = fizzBuzz.checkIfMultipleOfFive(5);
   expect(result).toBe('Buzz');
 });
